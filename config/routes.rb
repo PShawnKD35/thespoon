73a2ddcfce8fe0bb80c93c250724b8268780b4ea
Shawn Peng <shawn-peng@outlook.com>
2019-07-22 13:50:36 +0800

    added dynamic data to erb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   root to: 'pages#home'
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
