6a552b40bf181de56077ff8de5a10e4385fad1e3
Shawn Peng <shawn-peng@outlook.com>
2019-07-22 15:04:49 +0800

    Complete the the website

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,33 @@
 class RestaurantsController < ApplicationController
+  
+  # Let's fake a database records
+  RESTAURANTS = {
+    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+  }
+  
   def index
+    if params[:food_type] == nil
+      @restaurants = RESTAURANTS
+    else
+      @category = params[:food_type]
+      @restaurants = RESTAURANTS.select { |id, r| r[:category] == @category }
+    end
+  end
+
+  def create
+    name = params[:name]
+    address = params[:address]
+    category = params[:category]
+    RESTAURANTS[RESTAURANTS.length + 1] = { name: name, address: address, category: category }
+    render plain: "Add to DB restaurant '#{name}' with address '#{address}'"
+  end
+
+  def show
+    id = params[:id]
+    render plain: "Not an appropriate id: #{id}" unless id.match?(/\A[0-9]+\Z/)
+    id = id.to_i
+    render plain: "This id doesn't exists: #{id}" if RESTAURANTS[id].nil?
+    @restaurant = RESTAURANTS[id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # get 'pages/contact'
   # get 'pages/about'
+  root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
-  root to: 'pages#home'
   get 'restaurants', to: 'restaurants#index', as: :restaurants
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
